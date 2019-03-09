00501693e5124d7fde69d5d0d47a927468961ff0
Steven Petryk <steven.petryk@intercom.io>
2019-03-09 08:20:42 -0800

    Bundle up properly

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
 import sourceMaps from "rollup-plugin-sourcemaps"
-import camelCase from "lodash.camelcase"
 import typescript from "rollup-plugin-typescript2"
 import json from "rollup-plugin-json"
 
@@ -12,32 +11,23 @@ const libraryName = "contentful-typescript-codegen"
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: "umd", sourcemap: true },
+    { file: pkg.main, format: "cjs", sourcemap: true },
     { file: pkg.module, format: "es", sourcemap: true },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ["prettier", "lodash"],
   watch: {
     include: "src/**",
   },
   plugins: [
-    // Allow json resolution
     json(),
-    // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs({
       namedExports: {
         "node_modules/prettier/index.js": ["format", "resolveConfig"],
         "node_modules/lodash/lodash.js": ["camelCase", "upperFirst"],
       },
     }),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
     sourceMaps(),
   ],
 }
