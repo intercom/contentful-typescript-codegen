809eb0361667634b4044946808bc63caab0c2eb3
Steven Petryk <steven.petryk@intercom.io>
2019-11-26 11:47:36 -0800

    Improve test coverage

---
+++ test/renderers/contentful-fields-only/fields/renderRichText.test.ts
@@ -0,0 +1,20 @@
+import renderRichText from "../../../../src/renderers/contentful-fields-only/fields/renderRichText"
+import { Field } from "contentful"
+
+describe("renderRichText()", () => {
+  const simpleRichText: Field = {
+    type: "Object",
+    id: "fieldId",
+    name: "Field Name",
+    validations: [],
+    omitted: false,
+    required: true,
+    disabled: false,
+    linkType: undefined,
+    localized: false,
+  }
+
+  it("works", () => {
+    expect(renderRichText(simpleRichText).trim()).toMatchInlineSnapshot(`"any"`)
+  })
+})
---
+++ test/renderers/renderFieldsOnly.test.ts
@@ -0,0 +1,49 @@
+import renderFieldsOnly from "../../src/renderers/renderFieldsOnly"
+import { ContentType, Sys } from "contentful"
+
+describe("renderFieldsOnly()", () => {
+  it("renders given a content type", async () => {
+    const contentTypes: ContentType[] = [
+      {
+        sys: {
+          id: "myContentType",
+        } as Sys,
+        fields: [
+          {
+            id: "arrayField",
+            name: "Array field",
+            required: true,
+            validations: [{}],
+            items: {
+              type: "Symbol",
+              validations: [
+                {
+                  in: ["one", "of", "the", "above"],
+                },
+              ],
+            },
+            disabled: false,
+            omitted: false,
+            localized: false,
+            type: "Array",
+          },
+        ],
+        description: "",
+        displayField: "",
+        name: "",
+        toPlainObject: () => ({} as ContentType),
+      },
+    ]
+
+    expect(await renderFieldsOnly(contentTypes)).toMatchInlineSnapshot(`
+      "export interface IMyContentType {
+        fields: {
+          /** Array field */
+          arrayField: (\\"one\\" | \\"of\\" | \\"the\\" | \\"above\\")[];
+        };
+        [otherKeys: string]: any;
+      }
+      "
+    `)
+  })
+})
