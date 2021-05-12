9856ea5e37c5ef54c4ea07f9d441851cfd4f6aaf
Peeyush Singla <peeyush.singla@hey.car>
2021-05-12 16:01:50 +0200

    remove I prefix from interfaces

--- src/renderers/contentful/renderContentTypeId.ts
+++ src/renderers/contentful/renderContentTypeId.ts
@@ -1,5 +1,5 @@
 import { upperFirst, camelCase } from "lodash"
 
 export default function renderContentTypeId(contentTypeId: string): string {
-  return "I" + upperFirst(camelCase(contentTypeId))
+  return upperFirst(camelCase(contentTypeId))
 }
