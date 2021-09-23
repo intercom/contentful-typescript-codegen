3abb3ffa51c7e3917404b998b7618ddd6adbefda
Gabriel Anca Corral <GabrielAnca@users.noreply.github.com>
2021-09-23 10:38:10 +0200

    Remove unused import

--- src/renderers/contentful/renderAllLocales.ts
+++ src/renderers/contentful/renderAllLocales.ts
@@ -1,6 +1,5 @@
 import renderUnion from "../typescript/renderUnion"
 import { Locale } from "contentful"
-import { escapeSingleQuotes } from "../utils"
 
 export default function renderAllLocales(locales: Locale[]): string {
   return renderUnion(
