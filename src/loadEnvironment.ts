1209802210d85655806a24672e0fbcfe9a84dfd8
Gareth Jones <jones258@gmail.com>
2022-01-22 09:20:36 +1300

    feat: support `getContentfulEnvironment.ts`

---
+++ src/loadEnvironment.ts
@@ -0,0 +1,77 @@
+import * as path from "path"
+import * as fs from "fs"
+import { ContentTypeCollection, LocaleCollection } from "contentful"
+
+/* istanbul ignore next */
+const interopRequireDefault = (obj: any): { default: any } =>
+  obj && obj.__esModule ? obj : { default: obj }
+
+type Registerer = { enabled(value: boolean): void }
+
+let registerer: Registerer | null = null
+
+function enableTSNodeRegisterer() {
+  if (registerer) {
+    registerer.enabled(true)
+
+    return
+  }
+
+  try {
+    registerer = require("ts-node").register() as Registerer
+    registerer.enabled(true)
+  } catch (e) {
+    if (e.code === "MODULE_NOT_FOUND") {
+      throw new Error(
+        `'ts-node' is required for TypeScript configuration files. Make sure it is installed\nError: ${e.message}`,
+      )
+    }
+
+    throw e
+  }
+}
+
+function determineEnvironmentPath() {
+  const pathWithoutExtension = path.resolve(process.cwd(), "./getContentfulEnvironment")
+
+  if (fs.existsSync(`${pathWithoutExtension}.ts`)) {
+    return `${pathWithoutExtension}.ts`
+  }
+
+  return `${pathWithoutExtension}.js`
+}
+
+export interface ContentfulEnvironment {
+  getContentTypes(options: { limit: number }): Promise<ContentTypeCollection>
+  getLocales(): Promise<LocaleCollection>
+}
+
+export type EnvironmentGetter = () => Promise<ContentfulEnvironment>
+
+function getEnvironmentGetter(): EnvironmentGetter {
+  const getEnvironmentPath = determineEnvironmentPath()
+
+  if (getEnvironmentPath.endsWith(".ts")) {
+    enableTSNodeRegisterer()
+
+    return interopRequireDefault(require(getEnvironmentPath)).default
+  }
+
+  return require(getEnvironmentPath)
+}
+
+export async function loadEnvironment() {
+  try {
+    const getEnvironment = getEnvironmentGetter()
+    const environment = await getEnvironment()
+
+    return {
+      contentTypes: await environment.getContentTypes({ limit: 1000 }),
+      locales: await environment.getLocales(),
+    }
+  } finally {
+    if (registerer) {
+      registerer.enabled(false)
+    }
+  }
+}
---
+++ test/loadEnvironment.test.ts
@@ -0,0 +1,104 @@
+import * as fs from "fs"
+import { loadEnvironment } from "../src/loadEnvironment"
+
+const getContentfulEnvironmentFileFactory = jest.fn((_type: string) => {
+  return () => ({
+    getContentTypes: () => [],
+    getLocales: () => [],
+  })
+})
+
+jest.mock(
+  require("path").resolve(process.cwd(), "./getContentfulEnvironment.js"),
+  () => getContentfulEnvironmentFileFactory("js"),
+  { virtual: true },
+)
+
+jest.mock(
+  require("path").resolve(process.cwd(), "./getContentfulEnvironment.ts"),
+  () => getContentfulEnvironmentFileFactory("ts"),
+  { virtual: true },
+)
+
+const tsNodeRegistererEnabled = jest.fn()
+const tsNodeRegister = jest.fn()
+
+jest.mock("ts-node", () => ({ register: tsNodeRegister }))
+
+describe("loadEnvironment", () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.restoreAllMocks()
+
+    getContentfulEnvironmentFileFactory.mockReturnValue(() => ({
+      getContentTypes: () => [],
+      getLocales: () => [],
+    }))
+    tsNodeRegister.mockReturnValue({ enabled: tsNodeRegistererEnabled })
+  })
+
+  describe("when getContentfulEnvironment.ts exists", () => {
+    beforeEach(() => {
+      jest.spyOn(fs, "existsSync").mockReturnValue(true)
+    })
+
+    describe("when ts-node is not found", () => {
+      beforeEach(() => {
+        // technically this is throwing after the `require` call,
+        // but it still tests the same code path so is fine
+        tsNodeRegister.mockImplementation(() => {
+          throw new (class extends Error {
+            public code: string
+
+            constructor(message?: string) {
+              super(message)
+              this.code = "MODULE_NOT_FOUND"
+            }
+          })()
+        })
+      })
+
+      it("throws a nice error", async () => {
+        await expect(loadEnvironment()).rejects.toThrow(
+          "'ts-node' is required for TypeScript configuration files",
+        )
+      })
+    })
+
+    describe("when there is another error", () => {
+      beforeEach(() => {
+        tsNodeRegister.mockImplementation(() => {
+          throw new Error("something else went wrong!")
+        })
+      })
+
+      it("re-throws", async () => {
+        await expect(loadEnvironment()).rejects.toThrow("something else went wrong!")
+      })
+    })
+
+    describe("when called multiple times", () => {
+      it("re-uses the registerer", async () => {
+        await loadEnvironment()
+        await loadEnvironment()
+
+        expect(tsNodeRegister).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    it("disables the registerer afterwards", async () => {
+      await loadEnvironment()
+
+      expect(tsNodeRegistererEnabled).toHaveBeenCalledWith(false)
+    })
+  })
+
+  it("requires the javascript config", async () => {
+    jest.spyOn(fs, "existsSync").mockReturnValue(false)
+
+    await loadEnvironment()
+
+    expect(getContentfulEnvironmentFileFactory).toHaveBeenCalledWith("js")
+    expect(getContentfulEnvironmentFileFactory).not.toHaveBeenCalledWith("ts")
+  })
+})
